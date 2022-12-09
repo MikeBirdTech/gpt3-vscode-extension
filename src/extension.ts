37b2ff3f60e1038c41174175e1b37b92c829d301
Mike Bird <63524998+Arrendy@users.noreply.github.com>
2022-12-09 16:22:37 -0500

    Clean up (#3)
    * clean up marketplace presentation and code

--- src/extension.ts
+++ src/extension.ts
@@ -8,52 +8,54 @@ const getConfValue = <T = string>(key: string) => vscode.workspace.getConfigurat
 export async function activate(context: vscode.ExtensionContext) {
 	console.log('Activated');
 
-	let disposable = vscode.commands.registerCommand('GPT.createDocs', async () => {
+	let createDocumentation = vscode.commands.registerCommand('GPT.createDocs', async () => {
 		console.log('Running createDocs');
 		const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
-			const selectedText = editor.document.getText(editor.selection);
+		if (!editor) {
+			return;
+		}
+		const selectedText = editor.document.getText(editor.selection);
 
-			const prompt = `Create clear and concise documentation for the following code. 
+		const prompt = `Create clear and concise documentation for the following code. 
 			Every line must be commented out and use multiple lines for long sentences. 
 			Use best practices.
 			Code: ${selectedText}`;
 
-			const model = getConfValue('model');
-			const max_tokens = getConfValue<number>('maxTokens');
-			const temperature = getConfValue<number>('temperature');
-			
-			if (temperature < 0 || temperature > 1) {
-				vscode.window.showInformationMessage("Temperature must be between 0 and 1, please update your settings");
-				return;
-			}
+		const model = getConfValue('model');
+		const max_tokens = getConfValue<number>('maxTokens');
+		const temperature = getConfValue<number>('temperature');
 
-			const configuration = new Configuration({
-				organization: getConfValue('openaiOrg'),
-				apiKey: getConfValue('openaiApiKey'),
-			});
-
-			const openai = new OpenAIApi(configuration);
-
-			const response = await openai.createCompletion({
-				model,
-				prompt,
-				max_tokens,
-				temperature,
-			});
-
-			const output = response.data.choices[0].text?.trim();
-
-			// Start a new edit operation
-			editor.edit((editBuilder) => {
-				// Insert the text at the start of the selection
-				editBuilder.insert(editor.selection.start, `${output}\n`);
-			});
+		if (temperature < 0 || temperature > 1) {
+			vscode.window.showInformationMessage("Temperature must be between 0 and 1, please update your settings");
+			return;
 		}
-	});
 
-	context.subscriptions.push(disposable);
+		const configuration = new Configuration({
+			organization: getConfValue('openaiOrg'),
+			apiKey: getConfValue('openaiApiKey'),
+		});
+
+		const openai = new OpenAIApi(configuration);
+
+		const response = await openai.createCompletion({
+			model,
+			prompt,
+			max_tokens,
+			temperature,
+		});
+
+		const output = response.data.choices[0].text?.trim();
+
+		// Start a new edit operation
+		editor.edit((editBuilder) => {
+			// Insert the text at the start of the selection
+			editBuilder.insert(editor.selection.start, `${output}\n`);
+		});
+	}
+	);
+
+	context.subscriptions.push(createDocumentation);
 }
 
 // This method is called when your extension is deactivated
