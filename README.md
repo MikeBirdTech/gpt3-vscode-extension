7cf5d55ca14e047cf524a5326fa9513df634bd4d
Mike Bird <63524998+Arrendy@users.noreply.github.com>
2022-12-12 12:40:10 -0500

    Status bar item (#12)
    * create status bar item that launches Ask GPT function. Copyable, readable modal response for ask gpt and suggest improvement functions
    
    * store api keys in secret storage

--- README.md
+++ README.md
@@ -1,36 +1,43 @@
-# GPT Visual Studio Code Extension README
+# GPT-3 Visual Studio Code Extension
 Use GPT-3 to be a more efficient programmer!
 
-- Write to GPT - ask it anything directly from VS Code
-- Generate documentation for your code
-- Generate suggested alternatives for your code, including an explanation
+- ✏️ Write to GPT - ask it anything directly from VS Code
+- 📖 Generate documentation for your code
+- 💡 Generate suggested alternatives for your code, including an explanation
 
 ---
-
-## Requirements
-[Your OpenAI API Key](https://beta.openai.com/signup)
-
---- 
 
 ## Install the extension
 
 [Marketplace](https://marketplace.visualstudio.com/items?itemName=Arrendy.gpt3-vscode-extension)
 
 ---
+## Requirements
+Enter your [OpenAI API Key](https://beta.openai.com/signup) into a secret storage prompt.
+
+⚠️ **NOTE**: Some extensions ask you to store your API Key in settings.json - this is **not secure** ⚠️
+
+All extensions can read the entire contents of `settings.json`. If you store your API Key in plain text, a malicious extension can easily access it. To protect your API Key, you **must** store it in secret storage to prevent other extensions from accessing it.
+
+--- 
+
 ## Current functionality
 ### Ask GPT
 Opens a text input box and the value entered will be sent to GPT for processing. The response will appear in a modal.
-Default key binding set to `alt + g`
 
-Status bar item also runs this function.
+> Default key binding set to `alt + g`
+
+> Status bar item also runs this function.
 
 ### Create Documentation
 Generates documentation for highlighted code. It will automatically inject it above the highlighted code.
-Default key binding set to `alt + d`
+
+> Default key binding set to `alt + d`
 
 ### Suggest Improvements
 Sends highlighted code to GPT-3 where it will return suggested code as well as an explanation for why the suggestion is an improvement. The suggested code and explanation will appear in a modal.
-Default key binding set to `alt + i`
+
+> Default key binding set to `alt + i`
 
 
 
@@ -49,8 +56,12 @@ May need to re-configure credentials if updating from a version older than v0.6.
 
 ## Release Notes
 
+### 0.9.0
+API Key in secret storage
+
 ### 0.8.0
 Status bar item to initiate Ask GPT function. Returns more readable modal with a copy output button
+
 ### 0.7.0
 Optionally include Organization in request to GPT
 
--- src/extension.ts
+++ src/extension.ts
@@ -9,49 +9,101 @@ interface Config {
 	organization?: string
 }
 
+
 const getConfValue = <T = string>(key: string) => vscode.workspace.getConfiguration('GPT').get(key) as T;
 
-const initConfig = () => {
+const initConfig = async (context: vscode.ExtensionContext) => {
+	console.log('init');
+
+	let apiKey = await context.secrets.get('OPENAI_API_KEY');
+
+	if (!apiKey) {
+		console.log("API Key doesn't exist in secret storage");
+
+		apiKey = await setNewAPIKey(context);
+	}
+
+
 	const config: Config = {
 		"model": getConfValue('model'),
 		"max_tokens": getConfValue<number>('maxTokens'),
 		"temperature": getConfValue<number>('temperature'),
-		"apiKey": getConfValue('apiKey'),
+		"apiKey": apiKey,
 	};
 
-	const org = getConfValue('org');
+	let org = getConfValue('org');
 
 	if (org) {
 		config.organization = org;
 	}
 
-	if (!config.temperature || config.temperature < 0 || config.temperature > 1) {
-		vscode.window.showInformationMessage("Temperature must be between 0 and 1, please update your settings");
-		return;
-	}
-	if (!config.max_tokens || config.max_tokens < 1 || config.max_tokens >= 4000) {
-		vscode.window.showInformationMessage("Max tokens must be between 1 and 4000, please update your settings");
-		return;
-	}
-	if (!config.apiKey) {
-		vscode.window.showInformationMessage("OpenAI API Key missing, please update your settings");
-		return;
-	}
-	if (!config.model) {
-		vscode.window.showInformationMessage("GPT Model missing, please update your settings");
+	let { isValid, reason } = validateConfig(config);
+
+	if (!isValid) {
+		vscode.window.showErrorMessage(reason);
 		return;
 	}
 
 	return config;
 };
 
+const setNewAPIKey = async (context: vscode.ExtensionContext): Promise<string> => {
+	const inputBoxOptions = {
+		title: "Please enter your OpenAI API Key",
+		prompt: "Store your API Key in secret storage",
+		password: true,
+		ignoreFocusOut: true
+	};
+
+	const secret = await vscode.window.showInputBox(inputBoxOptions);
+
+	if (!secret) {
+		vscode.window.showWarningMessage('No API Key received.');
+
+		return "";
+	}
+
+	await context.secrets.store('OPENAI_API_KEY', secret);
+
+	return secret;
+};
+
+const validateConfig = (config: Config) => {
+	let reason = "";
+	let isValid = true;
+
+	if (config.apiKey === "") {
+		reason = "API Key cannot be an empty string, please reload window and enter API Key";
+		isValid = false;
+	}
+
+	if (!config.temperature || config.temperature < 0 || config.temperature > 1) {
+		reason = "Temperature must be between 0 and 1, please update your settings";
+		isValid = false;
+	}
+
+	if (!config.max_tokens || config.max_tokens < 1 || config.max_tokens >= 4000) {
+		reason = "Max tokens must be between 1 and 4000, please update your settings";
+		isValid = false;
+	}
+
+	if (!config.model) {
+		reason = "GPT Model missing, please update your settings";
+		isValid = false;
+	}
+	console.log(`isValid: ${isValid}, reason: ${reason}`);
+
+	return { isValid, reason };
+};
+
 // This method is called when the extension is activated
-// The extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
 	console.log('Activated');
 
-	const config = initConfig();
+	const config = await initConfig(context);
 	if (!config) {
+		deactivate();
+    
 		return;
 	}
 
@@ -225,10 +277,38 @@ export async function activate(context: vscode.ExtensionContext) {
 		statusBarItem.show();
 	});
 
+	// Update OpenAI API Key
+	let updateAPIKey = vscode.commands.registerCommand('GPT.updateAPIKey', async () => {
+		console.log('Running updateAPIKey');
+
+		statusBarItem.hide();
+		const statusMessage = vscode.window.setStatusBarMessage('$(heart) Securely storing your API Key $(pencil)');
+
+		await setNewAPIKey(context);
+
+		statusMessage.dispose();
+		statusBarItem.show();
+	});
+
+	// Remove OpenAI API Key
+	let removeAPIKey = vscode.commands.registerCommand('GPT.removeAPIKey', async () => {
+		console.log('Running removeAPIKey');
+
+		statusBarItem.hide();
+		const statusMessage = vscode.window.setStatusBarMessage('$(heart) Securely REMOVING your API Key $(error)');
+
+		await context.secrets.delete('OPENAI_API_KEY');
+
+		statusMessage.dispose();
+		statusBarItem.show();
+	});
+
 
 	context.subscriptions.push(createDocumentation);
 	context.subscriptions.push(suggestImprovement);
 	context.subscriptions.push(askGPT);
+	context.subscriptions.push(updateAPIKey);
+	context.subscriptions.push(removeAPIKey);
 }
 
 // This method is called when your extension is deactivated
